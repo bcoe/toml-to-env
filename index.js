dfc922337b9fa09aadb0bcc7481b513180a4dd2a
Rebecca Turner <me@re-becca.org>
2018-07-05 17:34:05 -0700

    fix: switch TOML parser, which adheres closer to the spec (#1)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
-const toml = require('toml')
+const TOML = require('@iarna/toml')
 
 module.exports = function (path, cb) {
   process.nextTick(() => {
     try {
-      const data = toml.parse(fs.readFileSync(path, 'utf-8'))
+      const data = TOML.parse(fs.readFileSync(path, 'utf-8'))
       return cb(null, data.environment || {})
     } catch (err) {
       return cb(err)
